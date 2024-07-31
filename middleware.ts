3b7d1a534a396be748d3156d858a0c2c622a8a8e
shayan.heidary <shayan.heidary@knime.com>
2024-07-31 18:25:16 +0200

    protect routes in middleware

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { COOKIE_NAME } from './utils/constants'
+
+export function middleware(request: NextRequest) {
+  console.log('req', request)
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    if (!request.cookies.has(COOKIE_NAME)) {
+      console.log(request.url)
+      return NextResponse.redirect(new URL('/signin', request.url))
+    }
+  }
+
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/'],
+}
