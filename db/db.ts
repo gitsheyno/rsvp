4ca7a997bf06744cc8575a59fd6d661b384a0b35
shayan.heidary <shayan.heidary@knime.com>
2024-07-31 12:38:47 +0200

    init repo - form actions - slots - data fetching - caching

--- db/db.ts
+++ db/db.ts
@@ -3,6 +3,8 @@ import { drizzle } from 'drizzle-orm/libsql'
 import { createClient } from '@libsql/client'
 import * as schema from './schema'
 
+console.log('hello', process.env.TURSO_CONNECTION_URL)
+
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
